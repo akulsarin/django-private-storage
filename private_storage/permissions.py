d83774c3dcc261853617c4ca8d60022cc416bfc9
Akul Sarin <akulsarin@berkeley.edu>
2022-08-01 20:58:58 +0530

    using knox auth checker

--- private_storage/permissions.py
+++ private_storage/permissions.py
@@ -2,28 +2,17 @@
 Possible functions for the ``PRIVATE_STORAGE_AUTH_FUNCTION`` setting.
 """
 import django
-from knox.models import AuthToken
-from knox import crypto
-
-def extract_user(headers):
-    auth_list = None
-    user = None
-    try:
-        auth_list = headers['Authorization'].split(' ')
-    except KeyError:
-        return user
-    if auth_list[0] != 'Token' or len(auth_list) != 2:
-        return user
-    token = auth_list[1]
-    digest = crypto.hash_token(token)
-    user = AuthToken.objects.get(digest=digest).user
-    return user
+from knox.auth import TokenAuthentication
 
 if django.VERSION >= (1, 10):
     def allow_authenticated(private_file):
-        user = extract_user(private_file.request.headers)
-        if not (user and user.is_active):
+        try:
+            authenticated = TokenAuthentication().authenticate(private_file.request)
+        except:
             return False
+        if not authenticated:
+            return False
+        user, _ = authenticated
         is_authenticated = (user.picture == private_file.relative_name)
         return is_authenticated
 
